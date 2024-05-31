da3a6b925de784c0699f942bd684b846ef1a01b9
LahiruEra <erandasiriwardhana@gmail.com>
2024-05-31 11:03:40 +0530

    Update SalaryCalculator.java
    add exercise as a comment

--- tau_java_course/src/chapter3/SalaryCalculator.java
+++ tau_java_course/src/chapter3/SalaryCalculator.java
@@ -1,7 +1,10 @@
 package chapter3;
 
 /* comment
+Let's say we have a team of salespeople, all of whom make a standard rate of $1,000 a week. For any of them who make more than 10 sales that week, they get an additional bonus of $250.
 
+So, the main path would be to pay a salesperson $1,000 - everyone gets this no matter what. 
+However, at some point we need to check to see if the condition was met of them making more than 10 sales. If it's met, then we need to add more to their payment.
 
   */
 
