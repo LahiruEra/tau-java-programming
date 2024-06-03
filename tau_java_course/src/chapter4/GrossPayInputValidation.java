8f9ce65cefd20e63bacba5135cbff2c24880576f
Lahiru <Lahiru@DESKTOP-MC99MSP>
2024-06-03 14:42:01 +0530

    Chapter 4 and 5

---
+++ tau_java_course/src/chapter4/GrossPayInputValidation.java
@@ -0,0 +1,37 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class GrossPayInputValidation {
+	
+	/*
+	 * WHILE LOOP:
+	 * Each store employee makes $15 an hour. Write a program that allows the employee
+	 * to enter the number of hours worked for the week. Do not allow overtime.
+	 * TAU chapter 4a
+	 */
+
+	public static void main(String[] args) {
+		// Initialize know variables
+		int rate = 15;
+		int maxHours = 40;
+		
+		//Get input for unknown variables
+		System.out.print("How many hours did you worked this week");
+		Scanner scanner = new Scanner(System.in);
+		double hoursWorked = scanner.nextDouble();
+		
+		//validate input
+		while(hoursWorked > maxHours || hoursWorked < 1) {
+			System.out.println("Invalid. Hours should be between 1 and 40. Try again");
+			hoursWorked = scanner.nextDouble();
+		}
+		scanner.close();
+		
+		//Calculate gross
+		double gross = rate * hoursWorked;
+		System.out.println("Gross pay: $"+gross);
+
+	}
+
+}
