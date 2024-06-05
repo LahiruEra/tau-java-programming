30037fdbdf31a129ada81a66300293a1f12c3a44
Lahiru <Lahiru@DESKTOP-MC99MSP>
2024-06-05 18:42:13 +0530

    Chapter 7-a,b,c

---
+++ tau_java_course/src/chapter7/LotteryTicket.java
@@ -0,0 +1,38 @@
+package chapter7;
+
+/* chapter7a
+ * create a Lottery Quick Pick application that will generate 
+ * a lottery ticket with 6 random numbers, between 1 and 69
+ */
+
+import java.util.Random;
+
+public class LotteryTicket {
+
+    private static final int LENGTH = 6;
+    private static final int MAX_TICKET_NUMBER = 69;
+
+    public static void main(String[] args){
+
+        int[] ticket = generateNumbers();
+        printTicket(ticket);
+    }
+
+    public static int[] generateNumbers(){
+
+        int[] ticket = new int[LENGTH];
+        Random random = new Random();
+
+        for(int i=0; i< LENGTH; i++){
+           ticket[i] = random.nextInt(MAX_TICKET_NUMBER) + 1;
+        }
+
+        return ticket;
+    }
+
+    public static void printTicket(int ticket[]){
+        for(int i=0; i<LENGTH; i++){
+            System.out.print(ticket[i] + " | ");
+        }
+    }
+}
