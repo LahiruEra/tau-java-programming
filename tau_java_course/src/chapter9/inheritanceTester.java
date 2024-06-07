7f8d289556fbd6fc2fbfba052cf632fc99f52d5b
Lahiru <Lahiru@DESKTOP-MC99MSP>
2024-06-07 15:33:44 +0530

    Merge branch 'master' of https://github.com/LahiruEra/tau-java-programming.git

---
+++ tau_java_course/src/chapter9/inheritanceTester.java
@@ -0,0 +1,19 @@
+package chapter9;
+
+//this is the class to test how the inheritance works
+
+public class inheritanceTester {
+
+	public static void main(String[] args) {
+		
+		Person person = new Person();
+		Employee employee = new Employee();
+		
+		//employee.getName();
+		//employee. will load all the methods including which defined under Parent cl
+		
+		
+
+	}
+
+}
