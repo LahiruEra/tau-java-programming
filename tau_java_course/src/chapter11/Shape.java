405f7381edcde32866204cb51929a09635335489
Lahiru <Lahiru@DESKTOP-MC99MSP>
2024-06-11 15:39:03 +0530

    chapter 11 a,b

---
+++ tau_java_course/src/chapter11/Shape.java
@@ -0,0 +1,14 @@
+package chapter11;
+
+//understand Abstract 
+
+public abstract class Shape {
+	
+	abstract double calculateArea();
+	
+	public void print() {
+		System.out.println("I'm a shape");	
+		
+	}
+
+}
