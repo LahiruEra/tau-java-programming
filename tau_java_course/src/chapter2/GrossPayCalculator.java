ddd6992799927ec2c3b3141f3158efa700320536
LahiruEra <erandasiriwardhana@gmail.com>
2024-06-19 07:10:27 +0530

    Update GrossPayCalculator.java

--- tau_java_course/src/chapter2/GrossPayCalculator.java
+++ tau_java_course/src/chapter2/GrossPayCalculator.java
@@ -6,7 +6,7 @@ public class GrossPayCalculator {
 	
 	public static void main(String arg[]) {
 		
-		//1. Get the number of hours worked
+		//1.Get the number of hours worked
 		
 		System.out.println("Enter  the number of hours the employee worked.");
 		Scanner scanner = new Scanner(System.in);
