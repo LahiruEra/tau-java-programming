7a071f799b9d14fea1232d6c745733d796ef55ca
LahiruEra <erandasiriwardhana@gmail.com>
2024-06-20 10:33:24 +0530

    Update Greetings.java

--- tau_java_course/src/chapter5/Greetings.java
+++ tau_java_course/src/chapter5/Greetings.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 /*
  * OUR FIRST METHOD
- * Write a method that asks a user for their name, then greets them by name.
+ * Write a method that asks a user for their name, then greets them by name
  */
 
 
