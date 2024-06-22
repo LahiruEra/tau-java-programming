d5834f353878477fb7d9749cd20f6db84cd8df00
LahiruEra <erandasiriwardhana@gmail.com>
2024-06-22 22:20:19 +0530

    Update AddNumbers.java

--- tau_java_course/src/chapter4/AddNumbers.java
+++ tau_java_course/src/chapter4/AddNumbers.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * DO WHILE LOOP
  * Write a program that allows a user to enter two numbers,
  * and then sums up the two numbers. The user should be able to
- * repeat this action until they indicate they are done.
+ * repeat this action until they indicate they are done
  */
 
 public class AddNumbers {
