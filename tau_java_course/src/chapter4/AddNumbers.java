887252796bf58fbcd0692a23f8db95bcd79437c4
LahiruEra <erandasiriwardhana@gmail.com>
2024-07-26 07:52:30 +0530

    Update AddNumbers.java

--- tau_java_course/src/chapter4/AddNumbers.java
+++ tau_java_course/src/chapter4/AddNumbers.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * Write a program that allows a user to enter two numbers,
  * and then sums up the two numbers. The user should be able to
  * repeat this action until they indicate they are done
- */
+ */ 
  
 public class AddNumbers {
 
