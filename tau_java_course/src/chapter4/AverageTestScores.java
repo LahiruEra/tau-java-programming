00c74f5391345a6a8d1811dca5f0f2445c953526
LahiruEra <erandasiriwardhana@gmail.com>
2024-07-26 07:52:45 +0530

    Update AverageTestScores.java

--- tau_java_course/src/chapter4/AverageTestScores.java
+++ tau_java_course/src/chapter4/AverageTestScores.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * NESTED LOOPS:
  * Find the average of each student's test scores.
  */
-  
+   
 public class AverageTestScores {
 
     public static void main(String args[]){
