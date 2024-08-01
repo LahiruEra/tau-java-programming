2b055da8b5076f602f0ae849810aeb87dcd45b1a
LahiruEra <erandasiriwardhana@gmail.com>
2024-08-01 08:04:11 +0530

    Update MonthConverter.java

--- tau_java_course/src/chapter6/MonthConverter.java
+++ tau_java_course/src/chapter6/MonthConverter.java
@@ -4,7 +4,7 @@ public class MonthConverter {
 
     public static void main(String args[]){
         System.out.println(Month.getMonth(2));
-        System.out.println(Month.getMonth("January"));
+        System.out.println(Month.getMonth("January")); 
         
     }
 }
