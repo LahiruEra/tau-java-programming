948de2d0abd0093d072b50e37ac4e022b1c21fcc
LahiruEra <erandasiriwardhana@gmail.com>
2024-08-14 08:18:58 +0530

    Update InstantCreditCheck.java

--- tau_java_course/src/chapter5/InstantCreditCheck.java
+++ tau_java_course/src/chapter5/InstantCreditCheck.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * Write an 'instant credit check' program that approves
  * anyone who makes more than $25,000 and has a credit score
  * of 700 or better. Reject all others.
- */
+ */ 
   
 public class InstantCreditCheck {
 
