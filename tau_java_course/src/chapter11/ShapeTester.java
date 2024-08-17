3ca7ff858bec4ff3ffcdfe8316207d437d28f5c9
LahiruEra <erandasiriwardhana@gmail.com>
2024-08-18 01:32:16 +0530

    Update ShapeTester.java

--- tau_java_course/src/chapter11/ShapeTester.java
+++ tau_java_course/src/chapter11/ShapeTester.java
@@ -5,7 +5,7 @@ public class ShapeTester {
 	public static void main(String[] args) {
 		  Shape rectangle = new Rectangle(5, 7);
 	      rectangle.print();
-	      System.out.println(rectangle.calculateArea());
+	      System.out.println(rectangle.calculateArea()); 
 	}
 
 }
