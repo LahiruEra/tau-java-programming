e7fd86cf48f9484dcfbf46df9f4d7a27ce3badd5
LahiruEra <erandasiriwardhana@gmail.com>
2024-08-22 10:47:03 +0530

    Update Product.java

--- tau_java_course/src/chapter11/Product.java
+++ tau_java_course/src/chapter11/Product.java
@@ -3,7 +3,7 @@ package chapter11;
 public interface Product {
 
 	double getPrice();
-	void setPrice(double price);
+	void setPrice(double price); 
 	
 	String getName();
 	void setName(String name);
