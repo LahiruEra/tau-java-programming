2c2ef51feb3348f5c3cc3bea67d5ea5292cae6c6
LahiruEra <erandasiriwardhana@gmail.com>
2024-08-26 06:06:29 +0530

    Update Cat.java

--- tau_java_course/src/chapter10/Cat.java
+++ tau_java_course/src/chapter10/Cat.java
@@ -5,7 +5,7 @@ public class Cat extends Animal {
     @Override 
     public void makeSound(){
         System.out.println("meow");
-    }
+    } 
 
     public void scratch(){
         System.out.println("I am a cat. I scratch things.");
