a23569e53265ed9668c38cfd721c7882d2af8653
LahiruEra <erandasiriwardhana@gmail.com>
2024-08-28 09:05:52 +0530

    Update HomeAreaCalculator.java

--- tau_java_course/src/chapter6/HomeAreaCalculator.java
+++ tau_java_course/src/chapter6/HomeAreaCalculator.java
@@ -3,7 +3,7 @@ package chapter6;
 /*
  * Write a class that creates instances of the `Rectangle` class to find the
  * total area of two rooms in a house.
- */
+ */ 
 public class HomeAreaCalculator {
 
     public static void main(String args[]){
