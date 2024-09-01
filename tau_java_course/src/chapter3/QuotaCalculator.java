9a126a8bc03f5394d30135a8faa19779a6045033
LahiruEra <erandasiriwardhana@gmail.com>
2024-09-01 08:53:12 +0530

    Update QuotaCalculator.java

--- tau_java_course/src/chapter3/QuotaCalculator.java
+++ tau_java_course/src/chapter3/QuotaCalculator.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * All salespeople are expected to make at least 10 sales each week.
  * For those who do, they receive a congratulatory message.
  * For those who don't, they are informed of how many sales they were short.
- */
+ */ 
 
 
 public class QuotaCalculator {
