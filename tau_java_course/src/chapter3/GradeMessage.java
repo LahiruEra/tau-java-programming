1e17362e75aedd06c15e81a937db64bd9463b8f8
LahiruEra <erandasiriwardhana@gmail.com>
2024-09-06 08:12:36 +0530

    Update GradeMessage.java

--- tau_java_course/src/chapter3/GradeMessage.java
+++ tau_java_course/src/chapter3/GradeMessage.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * SWITCH
  * Have a user enter their letter grade, and using a switch statement,
  * print out a message letting them know how they did
- */  
+ */   
 
 public class GradeMessage {
 
