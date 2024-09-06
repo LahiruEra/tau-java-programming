2b6b5c2d5c711d838d626839608a121854f38761
LahiruEra <erandasiriwardhana@gmail.com>
2024-09-06 08:12:44 +0530

    Update LoanQualifier.java

--- tau_java_course/src/chapter3/LoanQualifier.java
+++ tau_java_course/src/chapter3/LoanQualifier.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * NESTED IF:
  * To qualify for a loan, a person must make at least $30,000
  * and have been working at their current job for at least 2 years
- */ 
+ */
 
 public class LoanQualifier {
 
