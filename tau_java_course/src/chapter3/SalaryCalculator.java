ef91052931213f386b64f13c9fae807c5f3eb236
LahiruEra <erandasiriwardhana@gmail.com>
2024-09-11 07:56:43 +0530

    Update SalaryCalculator.java

--- tau_java_course/src/chapter3/SalaryCalculator.java
+++ tau_java_course/src/chapter3/SalaryCalculator.java
@@ -9,7 +9,7 @@ For any of them who make more than 10 sales that week, they get an additional bo
 So, the main path would be to pay a salesperson $1,000 - everyone gets this no matter what. 
 However, at some point we need to check to see if the condition was met of them making more than 10 sales. 
 If it's met, then we need to add more to their payment.
-  */
+  */ 
 
 public class SalaryCalculator {
 	
