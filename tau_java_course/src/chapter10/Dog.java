a820285abb581f6915f8e2ac2b0ed2fee37b6aa1
LahiruEra <erandasiriwardhana@gmail.com>
2024-09-23 07:55:52 +0530

    Update Dog.java

--- tau_java_course/src/chapter10/Dog.java
+++ tau_java_course/src/chapter10/Dog.java
@@ -6,7 +6,7 @@ public class Dog extends Animal {
     public void makeSound(){
         System.out.println("Woof");
     }
-   
+    
     
     public void fetch(){
         System.out.println("fetch is fun!");
